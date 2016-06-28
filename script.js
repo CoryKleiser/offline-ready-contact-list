16c869658f35b5a84f2c4627c337d6e14cade5c6
Andrew Lively <andrew.lively2@gmail.com>
2016-06-27 23:28:03 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function createContact(name, email, phone) {
+  // TODO: Create li element and return
+}
+
+function handleNewContactSubmit(ev) {
+  ev.preventDefault();
+
+  // TODO: Validate email and phone
+
+  // TODO: Save to offline storage (localStorage, IndexedDB, PouchDB)
+
+  // TODO: Create contact (li element)
+
+  // TODO: Append contact to ul#contactList
+}
+
+// TODO: Load contacts from offline storage (localStorage, IndexedDB, PouchDB)
+
+// TODO: Create contacts for each record
+
+// TODO: Append contacts (li elements) to ul#contactList
+
+// TODO: Add submit event listener to form#contactForm and use handleNewContactSubmit
